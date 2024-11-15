62a773a6aaa59fff891edfbae51129c0b9548d05
陈煜 <1871263099@qq.com>
2024-11-15 13:10:24 +0800

    image

---
+++ Python/Image/src/enlarge_memory.py
@@ -0,0 +1,49 @@
+"""
+扩大图片内存
+待升级
+无法确定升级到具体的大小
+只能多次
+pip install Pillow
+"""
+from PIL import Image
+import os
+
+
+def increase_image_size(input_path, output_path,target_size_mb,num=1):
+    # 打开原始图片
+    image = Image.open(input_path)
+
+    # 获取当前图片的大小
+    current_size = os.path.getsize(input_path) / (1024 * 1024)  # 以MB为单位
+    print(f"Current size: {current_size:.2f} MB")
+
+    # 计算所需的放大倍数
+    factor = (5*num / current_size) ** 0.5
+    new_size = (int(image.width * factor), int(image.height * factor))
+
+    # 调整图片大小
+    new_image = image.resize(new_size, Image.Resampling.LANCZOS)
+
+    # 保存新的图片
+    new_image.save(output_path, quality=100)  # quality参数调节图片质量
+    new_size_mb = os.path.getsize(output_path) / (1024 * 1024)  # 以MB为单位
+    print(new_size_mb,target_size_mb)
+    if new_size_mb < target_size_mb:
+        increase_image_size(input_path,output_path,target_size_mb,num+1)
+        return
+    print(f"New size: {new_size_mb:.2f} MB")
+
+
+if __name__ == "__main__":
+    # 输入图片路径和输出图片路径
+    input_path_folder = "../image/"
+    output_path_folder = "../image_res/"
+    # 目标图片大小（MB）
+    target_size_mb = 5
+
+    filenames = os.listdir(input_path_folder)
+    for i in filenames:
+        input_path = input_path_folder+i
+        output_path = output_path_folder+i
+        increase_image_size(input_path, output_path, target_size_mb)
+
--- Python/Study/src/Week08/chaoxing01/Test03.py
+++ Python/Study/src/Week08/chaoxing01/Test03.py
@@ -17,23 +17,22 @@ import  random
 def get_gcd(n1:int,n2:int) -> int:
     """
     最大公约数Greatest Common Divisor
-    欧几里得算法: 两个整数的最大公约数（GCD）与其中一个整数和这两个数的余数之间的GCD是相同的。
+    欧几里得算法: 两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数。
     """
     while n2 != 0:
-        t1 = n1
-        n1 = n2
-        n2 = n2 % t1
+        n1,n2 = n2,n2 % n1
     return n1
 def get_lcn(n1:int,n2:int) -> int:
     """最小公倍数Least Common Multiple"""
+    if n1 == 1 or n2 == 1:
+       return  max(n1,n2)
     return int(n1*n2/get_gcd(n1, n2))
 def get_gcd_and_lcn(n1:int,n2:int)->tuple[int,int]:
     return get_gcd(n1,n2),get_lcn(n1, n2)
 
 if __name__ == '__main__':
-    print(get_gcd(12,18))
-    print(get_gcd_and_lcn(12,18))
-    print(get_gcd_and_lcn(1, 8))
+    # print(get_gcd_and_lcn(12,18))
+    # print(get_gcd_and_lcn(1, 8))
     num1 =  int(random.random() * 100 + 1)
     num2= int(random.random() * 100 + 1)
     print(num1,num2,get_gcd_and_lcn(num1,num2))
