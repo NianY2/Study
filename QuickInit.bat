25f000d3ee898f2a267ebd38ab471b7e757b2dab
陈煜 <1871263099@qq.com>
2024-11-17 16:34:27 +0800

    quick lock in

--- QuickInit.bat
+++ QuickInit.bat
@@ -2,7 +2,7 @@ git remote rm origin
 @REM Gitee
 git remote add Gitee https://gitee.com/REMOTE_CY/study.git
 @REM Gihub
-git remote add Gihub https://github.com/NianY2/Study.git
+git remote add Github https://github.com/NianY2/Study.git
 @REM GitCode
 git remote add GitCode https://gitcode.com/qq_59636442/Study.git
 
